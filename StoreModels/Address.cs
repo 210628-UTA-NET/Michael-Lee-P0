f8644241ea22dac39b42b62ad85ebc82c0d36c78
Michael Lee <michael.lee03@email.saintleo.edu>
2021-06-30 15:48:51 -0400

    Inital Commit

---
+++ StoreModels/Address.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace StorModels
+{
+    public class Address
+    {
+        public string Name {get; set;}
+        public string Address {get; set;}
+        public string City {get; set;}
+        public string Province { get; set;}
+        public string Country {get; set;}
+        public string ZipCode {}
+
+    }
+}
