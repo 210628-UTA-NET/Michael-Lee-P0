43f3e1f5c115814735ad599398abf6ca00449fd5
Michael Lee <michael.lee03@email.saintleo.edu>
2021-07-08 17:03:08 -0400

    Placed Default in MenuFactory.cs

--- UI/MenuFactory.cs
+++ UI/MenuFactory.cs
@@ -5,27 +5,29 @@ using Microsoft.Extensions.Configuration;
 using BL;
 using DL;
 using DL.Entities;
+using System;
+
 namespace UI
 {
     //The class abstract for main and exports for the Factory.cs
     public class MenuFactory : IFactory
     {
-        //Get Configuration learned on 7/7/21
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory());
-            .AddJsonFile("appsetting.json");
-            .Build();
-
-        //Grabs our connectionString
-
-        string connectionString = configuration.GetConnectionString("Reference2DB");
-
-        DBContextOptions<DemoDbContext> options = new DbContextOpetionsBuilder<DemoDbContext>()
-            .UseSQLServer(connectionString)
-            .Options;
-            
-        public IMenu GetMenu(MenuType p_menu)
+        IMenu GetMenu(MenuType p_menu)
         {
+            /* //Get Configuration learned on 7/7/21
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory());
+                .AddJsonFile("appsetting.json");
+                .Build();
+
+            //Grabs our connectionString
+
+            string connectionString = configuration.GetConnectionString("Reference2DB");
+
+            DBContextOptions<DemoDbContext> options = new DbContextOpetionsBuilder<DemoDbContext>()
+                .UseSQLServer(connectionString)
+                .Options; */
+            
             switch (p_menu)
             {
                 case MenuType.MainMenu:// Bring the user to the Main Menu to instantiate the object. 
@@ -33,16 +35,23 @@ namespace UI
                 case MenuType.CustomerMenu:
                     return new CustomerMenu();
                 case MenuType.ShowCustomerMenu:
+                    return null;
                 //ShowRestaurantMenu needs a RestaurantBL object in the parameter because it depends on that object to be able to run its functionality
                 //BL needs the Repository object in the parameter because it depends on that object to be able to run
                 //This is call Dependency Injection
-                return new ShowCustomerMenu(new BL(new Repository()));
+
+                /* return new ShowCustomerMenu(new CustomerBL(new Repository(new FirstDatabaseContext()));
                 case MenuType.AddCustomerMenu:
-                    return new AddCustomerMenu(new BL(new Repository()));
+                    return new AddCustomerMenu(new CustomerBL(new Repository()));*/
                 default:
-                    return null;
+                    return null; 
 
             }
+        }
+
+        private void Build()
+        {
+            throw new NotImplementedException();
         }
     }
 }
