51de64edca648bd052852f9e39af7c752599ca6b
Michael Lee <michael.lee03@email.saintleo.edu>
2021-07-08 00:32:53 -0400

    StoreMenu Test

--- UI/StoreMenu.cs
+++ UI/StoreMenu.cs
@@ -4,8 +4,9 @@ namespace UI
 {
     public class StoreMenu : IMenu
     {
-        Console.WriteLine("[2] Enter the Store.");
-        Console.WriteLine("[1] View the Current List.");
+        Console.WriteLine("[3] Enter the Store.");
+        Console.WriteLine("[2] View the Current List.");
+        Console.WriteLine("[1] Review the Current List.")
         Console.WriteLine("[0] Return to the Main Menu.");
     }
 
