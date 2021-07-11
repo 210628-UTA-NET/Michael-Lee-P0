a46e92b479af61a2e2959723fe8764dc4a102776
Michael Lee <michael.lee03@email.saintleo.edu>
2021-07-11 13:56:15 -0400

    Added toString for Customers.cs

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,14 +1,17 @@
 using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
+using System.Collections.Generic; //This is for collections. 
+using System.Text.RegularExpressions; //Need this system for Regex. 
 namespace Models
 {
     public class Customer
     { 
         private string _firstName; //Private string for firstName 
         private string _lastName; //Private string for lastName
-        public Customer(){}
-        public int Id { get; set;}
+        private string address; //Private string for address
+        private string email; //Private string for email
+
+        public Customer(){} //No Args Constructor to build an object without parameters. 
+        public int Customer_Id { get; set;}
         public string FirstName
         { 
             get
@@ -42,18 +45,21 @@ namespace Models
 
         public string Address { get; set;}
         public string Email { get; set;}
-        public List<Order> ListofOrders {get; set;}
+        public List<Orders> ListofOrders {get; set;}
 
-        public Customer(string firstName, string lastName)
+        public Customer(string firstName, string lastName, string Address, string ListOfOrder)
 
         //You can override the default string functions, allowing to output the object parameters or receive and namespace.class COnstructor" 
         {
             FirstName = firstName;
             LastName = lastName;
+            Address = address;
+            Email = email;
         } 
          public override string ToString() 
         {
-            return FirstName + " " + LastName;
+            //return FirstName + " " + LastName;
+            return $"Customer ID: {Customer_Id}\n FirstName: {_firstName}\n LastName: {_lastName}\n Address: {address}\nEmail: {email}\nListofOrders: {ListofOrders}";
         }
     }
 }
