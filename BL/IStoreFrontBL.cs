c7476aa9d53bf01d10ca2a31491e059bb7aea287
Michael Lee <michael.lee03@email.saintleo.edu>
2021-07-11 14:12:38 -0400

    Created an IStoreFrontBL.cs

---
+++ BL/IStoreFrontBL.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using Models;
+
+namespace BL
+{
+    public interface IStoreFrontBL
+    {
+        /// <summary>
+        /// Gets all the restaurant from the database
+        /// </summary>
+        /// <returns>Returns a list of restaurants</returns>
+        //List<Storefront> GetAllStoreFront();
+
+        //StoreFront GetInventory(StoreFront p_storeFront);
+
+        //Storefront AddStore(Storefront p_storeFront);
+
+        //Storefront GetStorefront(Storefront p_storeFront);
+    }
+}
