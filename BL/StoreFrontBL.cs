01ed101463a5c59dfed354d7e6361c142441324b
Michael Lee <michael.lee03@email.saintleo.edu>
2021-07-11 14:13:05 -0400

    Created a StoreFrontBl.cs

---
+++ BL/StoreFrontBL.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+// namespace BL
+// {
+//     private IRepository _repo;
+//     public StoreFrontBL(IRepository p_repo)
+//     {
+//         p_repo = p_repo;
+//     }
+//     public List<StoreFront> GetStoreFronts()
+//     {
+
+//     }
+// }
