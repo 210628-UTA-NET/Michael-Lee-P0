c6d9e04500dc8fd03f2656826ed910cd60f9ce5f
Michael Lee <michael.lee03@email.saintleo.edu>
2021-07-11 19:03:13 -0400

    Created StoreFrontRepository.cs

--- DL/StoreFrontRepository.cs
+++ DL/StoreFrontRepository.cs
@@ -18,33 +18,36 @@ namespace DL
         }
         public Model.StoreFront AddStoreFront(Model.StoreFront p_storeFront)
         {
-            _context.StoreFront.(new DL.Entities.StoreFront
-            {
-                Id = p_storeFront.Store_Id,
-                StoreName = p_storeFront.StoreName,
-                Address = p_storeFront.Address,
-                Inventory = p_storeFront.Inventory,
-                ListofOrder = p_storeFront.ListofOrder_Id,
-            });
+            throw new System.NotImplementedException();
 
-            _context.SaveChanges();
-            return p_storeFront;
+            // _context.StoreFront(new Entity.StoreFront
+            // {
+            //     Id = p_storeFront.Store_Id,
+            //     StoreName = p_storeFront.StoreName,
+            //     Address = p_storeFront.Address,
+            //     Inventory = p_storeFront.Inventory,
+            //     ListofOrder = p_storeFront.ListofOrder_Id,
+            // }
+
+            //_context.SaveChanges();
+            //return p_storeFront;
         }
 
         public List<Model.StoreFront> GetAllStoreFront()
         {
+            throw new System.NotImplementedException();
             //Method Syntax way
-            return _context.StoreFront.Select(
-                store => 
-                    new Model.StoreFront()
-                    {
-                        Store_Id = store.Store_Id,
-                        StoreName = store.StoreName,
-                        Address = store.Address,
-                        Inventory = store.Inventory,
-                        ListofOrder_Id = store.ListofOrder_Id, 
-                    }
-            ).ToList();
+            // return _context.StoreFront.Select(
+            //     store => 
+            //         new Model.StoreFront()
+            //         {
+            //             Store_Id = store.Store_Id,
+            //             StoreName = store.StoreName,
+            //             Address = store.Address,
+            //             Inventory = store.Inventory,
+            //             ListofOrder_Id = store.ListofOrder_Id, 
+            //         }
+            // ).ToList();
         }
 
         public List<Customer> GetStoreFront()
