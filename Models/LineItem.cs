4c4b5b58a7927949904cceaa4477f2cd438cac6b
Michael Lee <michael.lee03@email.saintleo.edu>
2021-07-11 14:33:58 -0400

    Changed LineItems.cs

--- Models/LineItem.cs
+++ Models/LineItem.cs
@@ -7,7 +7,9 @@ namespace Models
     public class LineItem
     {
     private int _quantity;
-        public Product Product {get; set;}
+
+    private int LineItem_Id; 
+        public string Product {get; set;}
         public int Quantity
         {
             get
@@ -23,5 +25,17 @@ namespace Models
                 _quantity = value;
             }
         }
-    } 
-}
+        public LineItem(){} //No arguements Constructor, no semicolon. 
+        public LineItem(int p_LineItem_Id, string p_product, int p_quantity)
+        //You can override the default string functions, allowing to output the object parameters or receive and namespace.class COnstructor" 
+        {
+            LineItem_Id = p_LineItem_Id;
+            Product = p_product; 
+            Quantity = p_quantity; 
+        } 
+         public override string ToString() //Add a two String method: another way for this class to create an object (create information to put in my database.)
+        {
+            return $"LineItem ID: {LineItem_Id}\n Product: {Product}\n Quantity: {Quantity}";
+        }
+    }
+} 
