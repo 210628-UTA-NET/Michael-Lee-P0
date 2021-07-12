5e9fde907e22d9b369bf309068403fdcf72fda81
Michael Lee <michael.lee03@email.saintleo.edu>
2021-07-11 20:25:48 -0400

    Updated Order.cs and Product.cs DL

--- DL/Entities/Order.cs
+++ DL/Entities/Order.cs
@@ -7,11 +7,14 @@ namespace DL.Entities
 {
     public partial class Order
     {
+        internal int p_order_Id;
+
         public int OrderId { get; set; }
         public string OrderLineItems { get; set; }
         public decimal? TotalPrice { get; set; }
 
         public virtual StoreFront Order1 { get; set; }
         public virtual Customer OrderNavigation { get; set; }
+        public object Order_Id { get; internal set; }
     }
 }
--- DL/Entities/Product.cs
+++ DL/Entities/Product.cs
@@ -11,5 +11,7 @@ namespace DL.Entities
         public string ProductName { get; set; }
         public decimal? ProductPrice { get; set; }
         public string Description { get; set; }
+        public int Product_Id { get; internal set; }
+        public string Product_Name { get; internal set; }
     }
 }
