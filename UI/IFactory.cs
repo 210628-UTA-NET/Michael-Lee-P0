33c308d4c7bb91a7fa43b33ed8bee6a4b336ace3
Michael Lee <michael.lee03@email.saintleo.edu>
2021-07-13 08:30:59 -0400

    Added Comment to IFactory.cs

--- UI/IFactory.cs
+++ UI/IFactory.cs
@@ -1,9 +1,8 @@
 namespace UI //IFactory interface will have one method.
 {
-    public interface IFactory
+    public interface IFactory //Creating this interface will help get a good layout for the class being implemented. 
     {
         //IMenu will produce a lot of Menues. 
         IMenu GetMenu(MenuType p_menu); //Needs a parameter of what I want and what MenuType I need.  
-    
     }
 }
